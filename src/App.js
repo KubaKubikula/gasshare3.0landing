defae4395664fd7a620b27ff62c35cf008e2769b
Kubakubikula <jakub.zient@gmail.com>
2022-02-01 17:16:00 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./phone.png";
+import playstore from "./playstore.png";
+import "./App.css";
+import { Container, Row, Col, Nav } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Container>
+                <Row>
+                    <Col>
+                        <Nav activeKey="/home" className="justify-content-end">
+                            <Nav.Item>
+                                <Nav.Link href="/home">Home</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="link-1">Shop</Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>Some part</Col>
+                    <Col className="phoneMiddle">
+                        <img src={logo} className="App-logo" alt="logo" />
+                    </Col>
+                    <Col className="rightColumn">
+                        Hhiker app - ridesharing app
+                        <br />
+                        <a href="https://play.google.com/store/apps/details?id=ttpc.hitchhikerapp">
+                            <img
+                                src={playstore}
+                                className="playstore"
+                                alt="logo"
+                            />
+                        </a>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
